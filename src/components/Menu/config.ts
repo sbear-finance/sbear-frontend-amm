62e9d2f888f343067ff2806a5623c16094ef5467
Erdem <hbearstake@yandex.com>
2021-04-03 15:34:08 +0300

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -27,7 +27,7 @@ const config: MenuEntry[] = [
     href: 'https://www.goosedefi.com/farms'
   },
   {
-    label: 'Nests',
+    label: 'Stake',
     icon: 'PoolIcon',
     href: 'https://www.goosedefi.com/nests'
   },
