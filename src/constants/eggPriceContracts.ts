4f78ca903ebf9ebb1a23ef4046b0ad8471b88912
Erdem <hbearstake@yandex.com>
2021-04-03 15:23:18 +0300

    -

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,6 +1,6 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  cakeAddress: '0x480713711C68aCfC6341Df8c53ef32f83990cd05',
+  busdAddress: '0x576406e8710F37dD071b28D8A6B2F8aE7b898CdD',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
 
